1847f94a880b0a358c72193eee9d516c5f9114b5
Mex777 <fologeaalexandru@gmail.com>
2022-05-18 18:05:14 +0300

    made the navbar

--- src/page.js
+++ src/page.js
@@ -1,17 +1,46 @@
-import Image from './pic.jpg';
 import './style.css';
+
+const title = (content) => {
+  const div = document.createElement('div');
+  div.className = 'title';
+
+  const title = document.createElement('h1');
+  title.textContent = content;
+
+  div.appendChild(title);
+  return div;
+};
+
+export const nav = (() => {
+  const div = document.createElement('div');
+  div.className = 'nav';
+
+  const btns = document.createElement('div');
+  btns.className = 'btns';
+  div.appendChild(btns);
+
+  const addBtn = (name) => {
+    const btn = document.createElement('button');
+    btn.textContent = name;
+    btns.appendChild(btn);
+  };
+
+  const element = () => div;
+
+  return {element, addBtn};
+})();
 
 const main = () => {
   const container = document.createElement('div');
   container.className = 'container';
 
-  const nav = document.createElement('div');
-  nav.className = 'nav';
-  const menuBtn = document.createElement('button');
-  const contactBtn = document.createElement('button');
-  nav.append(menuBtn, contactBtn);
+  nav.addBtn('Acasa');
+  nav.addBtn('Meniu');
+  nav.addBtn('Galerie');
+  nav.addBtn('Contact');
 
-  container.appendChild(nav);
+  container.appendChild(nav.element());
+  container.appendChild(title('Nea Ion Restaurant'));
   return container;
 };
 
---
+++ src/pageTempl.js
@@ -0,0 +1,5 @@
+const page = (name) => {
+  const div = document.createElement('div');
+
+  const title = document.createElement('h1');
+};
